ed6ffa93b3c331c95047e034b59884fead4f0e46
Liam P <57645526+MineMaster836@users.noreply.github.com>
2022-01-12 23:40:54 -0500

    Added file for token as in token.txt

--- src/main/java/me/anccalculator/Main.java
+++ src/main/java/me/anccalculator/Main.java
@@ -6,10 +6,29 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) throws Exception {
-        JDA jda = JDABuilder.createDefault("token")
+        String token = null;
+
+        try {
+            File file = new File("token.txt");
+            Scanner scanner = new Scanner(file);
+            token = scanner.nextLine();
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            FileWriter writer = new FileWriter("token.txt");
+            writer.write("[add bot token here] (then restart the bot and remove all text except for bot token)");
+            writer.close();
+        }
+
+        JDA jda = JDABuilder.createDefault(token)
                 .disableCache(CacheFlag.VOICE_STATE)
                 .setActivity(Activity.watching("the money rain"))
                 .addEventListeners(new Commands())
